727d08deae9f3aeb2537f63cb0058494086d82b6
Gauri Manjith <gaurimanjith@gmail.com>
2025-07-03 05:58:46 +0000

    Clean copy of tracker-1 without large or nested files

---
+++ tracker-1/disable_tensorboard.py
@@ -0,0 +1,8 @@
+
+class DummySummaryWriter:
+    def __init__(self, *args, **kwargs):
+        pass
+    def add_scalar(self, *args, **kwargs): pass
+    def add_image(self, *args, **kwargs): pass
+    def add_graph(self, *args, **kwargs): pass
+    def close(self): pass
